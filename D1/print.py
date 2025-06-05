c40d0836a8a3e363688935c88f7d992c1481c6d8
Juan Manuel <116386643+juan-vazquez-20@users.noreply.github.com>
2025-06-04 22:21:56 -0600

    D1
    Signed-off-by: juan-vazquez-20 <juan.vazquez0290@alumnos.udg.mx>

---
+++ D1/print.py
@@ -0,0 +1,16 @@
+print("hola mundo")
+print(555+5)
+print('hola'+' '+'juan')
+print("me llamo\'juan\'")
+print("dos lineas \n separadas")
+print("\t la tabulacion")
+print('this isn\'t a number')
+print('este signo es \\ es una barar invertida')
+
+
+print("Ingresa tu nombre: ")
+print(input("nombre"))
+print("dame tu apellido")
+print("tu apellido es " + input(""))
+
+print("El nombre de tu cervesa es "+ input("Dime tu color favorito: ") + " " + input("cancion favorita:") + "\n Felicitaciones")
